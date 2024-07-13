5e508fb3fd8be3369eea9b09e14ed4374e03a52e
paullaster <paullasterokoth98@gmail.com>
2024-07-13 17:34:12 +0300

    add validators

--- app/controller/applications/application.controller.js
+++ app/controller/applications/application.controller.js
@@ -32,12 +32,14 @@ export class ApplicationController {
                 return res.ApiResponse.error(500, "Error while submitting application",);
             }
             // VALIDATIONS
-            const { success:ed } = await validationMiddleware.education(req.body.education);
+            const { success:ed, message, error } = await validationMiddleware.education(req.body.education);
+            console.log(ed, message, error);
             if (!ed) {
                 return res.ApiResponse.error(400, "Error while submitting application. Invalid education record.: ");
             }
             if (req.body.professionalBodys?.length) {
-                const { success:org} = await validationMiddleware.professionalBody(req.body.professionalBodys);
+                const { success:org, message:msg, error:err} = await validationMiddleware.professionalBody(req.body.professionalBodys);
+                console.log(org, msg, err);
             if (!org) {
                 return res.ApiResponse.error(400, "Error while submitting application. Invalid professional bodies record.: ");
             }
--- app/middleware/validation.middleware.js
+++ app/middleware/validation.middleware.js
@@ -1,40 +1,120 @@
-class Validator{
+class Validator {
     async education(educationArray) {
         try {
             if (!Array.isArray(educationArray)) {
-                return { success: false}
+                return { success: false }
             }
             let isValid = true;
             educationArray.forEach((education) => {
-                for (const prop in education) {
-                    if (education[prop] === undefined || education[prop] === "" || education[prop] === " ") {
-                        isValid = false;
-                        break;
+                const educationLevel = [
+                    {
+                        description: 'PHD',
+                        code: 'PHD'
+                    },
+                    {
+                        description: 'Masters',
+                        code: 'MASTERS'
+                    },
+                    {
+                        description: 'Bachelors',
+                        code: 'BACHELORS'
+                    },
+                    {
+                        description: 'Diploma',
+                        code: 'DIPLOMA'
+                    },
+                    {
+                        description: 'Certificate',
+                        code: 'CERTIFICATE'
+                    }
+                ];
+                const na = /N([^a-zA-Z0-9])A/;
+                const space = /\s/
+                let emptySpace = false
+
+                const edLevel = educationLevel.findIndex(levelObject => levelObject.code === education.educationLevel);
+                if (edLevel === -1) {
+                    isValid = false;
+                    return { success: false, message: "Invalid OPTION" };
                 }
-            }
+                if (typeof education.yearOfStart !== 'number' || typeof education.yearOfGraduation !== 'number') {
+                    isValid = false;
+                    return { success: false, message: "NAN" };
+                }
+                for (const prop in education) {
+                    if (!education[prop]) {
+                        isValid = false;
+                        return { success: false, message: "INVALID" };
+                    }
+                    if (education[prop] === "NA") {
+                        isValid = false;
+                        return { success: false, message: "NA" };
+                    }
+                    if (education[prop] === "undefined" ||education[prop] === "NULL") {
+                        isValid = false;
+                        return { success: false, message: "NA" };
+                    }
+                    if (na.test(education[prop])) {
+                        isValid = false;
+                        return { success: false, message: "NA" };
+                    }
+                    if (typeof education[prop] === "string") {
+                        education[prop]
+                            .trim()
+                            .split('')
+                            .forEach((c) => {
+                                if (space.test(c)) {
+                                    emptySpace = true;
+                                }
+                            })
+                    }
+                    if (emptySpace) {
+                        isValid = false;
+                        return { success: false, message: "space is empty" };
+                    }
+                }
             })
-            return { success: isValid};
+            return { success: isValid, message: "" };
         } catch (error) {
-            return {success: false, error: error.message};
+            return { success: false, error: error.message };
         }
     }
     async professionalBody(body) {
         try {
             if (!Array.isArray(body)) {
-                return { success: false}
+                return { success: false }
             }
             let isValid = true;
+            const space = /\s/;
+            const na = /N([^a-zA-Z0-9])A/;
+            let emptySpace = false
             body.forEach((org) => {
                 for (const prop in org) {
-                    if (org[prop] === undefined || org[prop] === "" || org[prop] === " ") {
+                    if (org[prop] === "NA") {
                         isValid = false;
-                        break;
+                        return { success: false, message: "NA" };
+                    }
+                    if (na.test(org[prop])) {
+                        isValid = false;
+                        return { success: false, message: "NA" };
+                    }
+                    org[prop]
+                        .trim()
+                        .split('')
+                        .forEach((c) => {
+                            if (space.test(c)) {
+                                emptySpace = true;
+                            }
+                        })
+                    if (emptySpace) {
+                        isValid = false;
+                        return { success: false, message: "EMPTY SPACE" };
+                    }
                 }
-            }
             })
-            return { success: isValid};
+            return { success: isValid };
         } catch (error) {
-            return {success: false, error: error.message};
+            return { success: false, error: error.message };
         }
     }
 }
