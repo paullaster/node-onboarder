5307673c05d43a1cb3a0ca96365744c5d78bdf3e
paullaster <paullasterokoth98@gmail.com>
2024-08-09 10:46:30 +0300

    refactor: refactored emailing from async to promise to ensure email complete or failed events are only emitted when email is actually is sent

--- app/events/listeners/account.activation.js
+++ app/events/listeners/account.activation.js
@@ -3,11 +3,11 @@ import { Notification } from "../../notification/notification.js";
 import User from "../../model/user.js";
 
 // APPLICATION SUBITTED
-eventEmmitter.on("activate-account", async(payload) => {
+eventEmmitter.on("activate-account", async (payload) => {
     try {
         console.log("activate-account emmitted");
-    const subject = "Consortium Account Activation";
-    const mailBody = `
+        const subject = "Consortium Account Activation";
+        const mailBody = `
                   <p style="font-family: sans-serif; font-size: 16px; line-height: 1.5; margin: 0 0 20px; color: #333">
                   Dear  ${payload.name},
                 </p>
@@ -27,23 +27,24 @@ eventEmmitter.on("activate-account", async(payload) => {
                   </p>
                 </footer>
     `;
-    const notify = new Notification(payload.email, subject, mailBody);
-    const {success} = await notify.via('viaEmail');
-    const getUser = await User.findOne({where: {email: payload.email}});
-    console.log(payload.resetLink);
-    if (success) {
-        if (getUser) {
-            await getUser.update({emailed: true});
-            eventEmmitter.emit('complete-email');
-        }
-    }
-    else {
-        if (getUser) {
-            await getUser.destroy();
-        }
-        eventEmmitter.emit('email-failed');
-    }
+        const notify = new Notification(payload.email, subject, mailBody);
+        notify.via('viaEmail').then((success) => {
+            User.findOne({ where: { email: payload.email } }).then(async (getUser) => {
+                if (success) {
+                    if (getUser) {
+                        await getUser.update({ emailed: true });
+                        eventEmmitter.emit('complete-email');
+                    }
+                }
+                else {
+                    if (getUser) {
+                        await getUser.destroy();
+                    }
+                    eventEmmitter.emit('email-failed');
+                }
+            });
+        });
     } catch (error) {
-        console.log({error: error.message});
+        console.log({ error: error.message });
     }
 });
